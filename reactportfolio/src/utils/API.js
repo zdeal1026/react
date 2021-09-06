e9c9fa321de5d36a67bafc95f02961bd5f855fa8
Zdeal <jedimastershake@gmail.com>
2021-09-05 23:54:17 -0400

    api added if wanted to use

---
+++ reactportfolio/src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+const BASEURL = '';
+const APIKEY = '';
+
+export default {
+    search: function(query) {
+        return axios.get(BASEURL + query + APIKEY);
+    }
+};
