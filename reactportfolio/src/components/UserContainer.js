a83adf33df8fb8fe57e6d310c64e32b287bac07f
Zdeal <jedimastershake@gmail.com>
2021-09-10 21:42:54 -0400

    usercontainer reconfiguration

--- reactportfolio/src/components/UserContainer.js
+++ reactportfolio/src/components/UserContainer.js
@@ -1,75 +1,136 @@
 import React, { Component } from "react";
-import Container from "./Container";
-import Row from "./Row";
-import Col from "./Col";
-import Card from "./Card";
-import SearchForm from "./SearchForm";
-import UserDetail from "./UserDetail";
-import API from "../utils/API";
+import SearchBar from "../SearchBar";
+import EmployeeTable from "../EmployeeTable";
+import API from "../../utils/API";
 
 class UserContainer extends Component {
   state = {
-    result: {},
-    search: ""
+    search: "",
+    employees: [],
+    filteredEmployees: [],
+    sortDirections: this.initialSortDirections,
   };
 
-  // When this component mounts, search for the movie "The Matrix"
-  componentDidMount() {
-    this.searchUser("");
+  get initialSortDirections() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+      dob: "",
+    };
   }
 
-  searchUser = query => {
-    API.search(query)
-      .then(res => this.setState({ result: res.data }))
-      .catch(err => console.log(err));
-  };
+  // When this component mounts, load random users as employees from https://randomuser.me/
+  componentDidMount() {
+    API.getEmployees()
+      .then((res) =>
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
 
-  handleInputChange = event => {
+  // Update search state to filter by employee name
+  handleInputChange = (event) => {
     const value = event.target.value;
-    const name = event.target.name;
-    this.setState({
-      [name]: value
-    });
+    this.setState({ search: value });
+    this.filterEmployees(value.toLowerCase().trim());
   };
 
-  // When the form is submitted, search the OMDB API for the value of `this.state.search`
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
-    this.searchUser(this.state.search);
+  };
+
+  // Sort with the key of specified object.
+  // If key has children, sort by primary child and optionally a secondary child. i.e. sort by last name, then first.
+  sortBy = (key, primary = 0, secondary = 0) => {
+    let sortedEmployees = this.state.filteredEmployees;
+    if (this.state.sortDirections[key]) {
+      this.setState({
+        filteredEmployees: sortedEmployees.reverse(),
+        sortDirections: {
+          ...this.initialSortDirections,
+          [key]: this.state.sortDirections[key] === "asc" ? "desc" : "asc",
+        },
+      });
+    } else {
+      sortedEmployees = this.state.filteredEmployees.sort((a, b) => {
+        a = a[key];
+        b = b[key];
+
+        // If secondary comparison given and primary comparison is equal
+        // Example: Sorting by last name, if last names are equal, then sort that instance by first name instead.
+        if (primary) {
+          if (secondary && a[primary] === b[primary]) {
+            return a[secondary].localeCompare(b[secondary]);
+          }
+          return a[primary].localeCompare(b[primary]);
+        } else {
+          return a.localeCompare(b);
+        }
+      });
+
+      this.setState({
+        filteredEmployees: sortedEmployees,
+        sortDirections: {
+          ...this.initialSortDirections,
+          [key]: "asc",
+        },
+      });
+    }
+  };
+
+  filterEmployees = (input) => {
+    if (input) {
+      this.setState({
+        filteredEmployees: this.state.employees.filter((employee) => {
+          return (
+            employee.name.first
+              .toLowerCase()
+              .concat(" ", employee.name.last.toLowerCase())
+              .includes(input) ||
+            employee.phone.includes(input) ||
+            employee.phone.replace(/[^\w\s]/gi, "").includes(input) ||
+            employee.email.includes(input) ||
+            this.formatDate(employee.dob.date).includes(input)
+          );
+        }),
+      });
+    } else {
+      this.setState({ filteredEmployees: this.state.employees });
+    }
+  };
+
+  formatDate = (date) => {
+    date = new Date(date);
+    let dob = [];
+    dob.push(("0" + (date.getMonth() + 1)).slice(-2));
+    dob.push(("0" + date.getDate()).slice(-2));
+    dob.push(date.getFullYear());
+
+    // Join formatted date
+    return dob.join("-");
   };
 
   render() {
     return (
-      <Container>
-        <Row>
-          <Col size="md-8">
-            <Card
-              heading={this.state.result.Title || "Search for a Player to Begin"}
-            >
-              {this.state.result.Title ? (
-                <UserDetail
-                  //title={this.state.result.Title}
-                  //src={this.state.result.Poster}
-                  //director={this.state.result.Director}
-                  //genre={this.state.result.Genre}
-                  //released={this.state.result.Released}
-                />
-              ) : (
-                <h3>No Results to Display</h3>
-              )}
-            </Card>
-          </Col>
-          <Col size="md-4">
-            <Card heading="Search">
-              <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-              />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <>
+        <SearchBar
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <div className="container mt-4">
+          <EmployeeTable
+            state={this.state}
+            sortBy={this.sortBy}
+            filterEmployees={this.filterEmployees}
+            formatDate={this.formatDate}
+          />
+        </div>
+      </>
     );
   }
 }
