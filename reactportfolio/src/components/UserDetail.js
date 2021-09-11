28127fd72c306987e8c6e5fa40d324342f92fd96
Zdeal <jedimastershake@gmail.com>
2021-09-10 21:46:01 -0400

    userdetail changed

--- reactportfolio/src/components/UserDetail.js
+++ reactportfolio/src/components/UserDetail.js
@@ -1,14 +1,54 @@
 import React from "react";
+import "./style.css"
 
-function UserDetail(props) {
+const EmployeeTable = (props) => {
   return (
-    <div className="text-center">
-      <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-      <h3>: {props.director}</h3>
-      <h3>Genre: {props.genre}</h3>
-      <h3>Released: {props.released}</h3>
-    </div>
-  );
-}
+    <table className="table table-striped table-sortable text-center">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col" data-field="name" data-sortable="true">
+            <span onClick={() => props.sortBy("name", "last", "first")}>
+              Name
+            </span>
+          </th>
+          <th scope="col">
+            <span onClick={() => props.sortBy("phone")}>Phone</span>
+          </th>
+          <th scope="col">
+            <span onClick={() => props.sortBy("email")}>Email</span>
+          </th>
+          <th scope="col">
+            <span onClick={() => props.sortBy("dob", "date")}>DOB</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.state.filteredEmployees.map((employee) => {
+          const { first, last } = employee.name;
+          const fullName = `${first} ${last}`;
 
-export default UserDetail;
+          // Format date
+          const dob = props.formatDate(employee.dob.date);
+
+          return (
+            <tr key={employee.login.uuid}>
+              <td>
+                <img src={employee.picture.thumbnail} alt={fullName} />
+              </td>
+              <td className="align-middle">{fullName}</td>
+              <td className="align-middle">
+              <a href={`tel:+1${employee.phone}`}>{employee.phone}</a></td>
+              <td className="align-middle email">
+                <a href={`mailto:${employee.email}`}>{employee.email}</a>
+              </td>
+              <td className="align-middle">{dob}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default EmployeeTable;
