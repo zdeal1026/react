de62f1f2983eb1833797d12bf1057520afe1fa9d
Zdeal <jedimastershake@gmail.com>
2021-09-12 21:04:16 -0400

    continued work on usercontainer

--- reactportfolio/src/components/Container/UserContainer.js
+++ reactportfolio/src/components/Container/UserContainer.js
@@ -42,6 +42,36 @@ class UserContainer extends Component {
     e.preventDefault();
   };
 
+  sortBy =(key, primary = 0, secondary = 0) => {
+    let sortedStaff = this.state.filteredEmployees;
+    if(this.state.sortDir[key]) {
+      this.setState({
+        filteredEmployees: sortedStaff.reversal(),
+        sortDir:{
+          ...this.initialSortDir,
+          [key]: this.state.sortDir[key] === "asc"?"desc": "asc",
+        },
+      });
+    } else {
+      sortedStaff = this.state.filteredEmployees.sor((a,b) => {
+        a = a[key];
+        b = b[key];
+
+        if(primary){
+          if(secondary && a[primary] === b[primary]) {
+            return a[secondary].localeCompare(b[secondary]);
+          }
+          return a[primary].localeCompare(b[primary]);
+          } else {
+            return a.localeCompare(b);
+          }
+      });
+
+      
+
+    }
+  }
+
   
   )
 }
